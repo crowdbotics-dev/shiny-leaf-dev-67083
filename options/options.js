9080d874721470f1d00d64b6ac3a35d7fa577540
Crowdbotics <engineering@crowdbotics.com>
2023-03-01 04:27:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-leaf-dev-67083",
+  url: "https://shiny-leaf-dev-67083.botics.co",
+  api: "https://shiny-leaf-dev-67083.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
